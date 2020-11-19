7eb83c19b2cffd159a94732265e69007f22db076
Jian Ma <jianquanma@gmail.com>
2020-11-19 17:09:18 -0500

    other files will be added to the repo as well

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Demo_fetchData
+//
+//  Created by Jian Ma on 11/19/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
