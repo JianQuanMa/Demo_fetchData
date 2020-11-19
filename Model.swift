5e57d67724188c562468f16956afa05bddc62f9e
Jian Ma <jianquanma@gmail.com>
2020-11-19 17:28:40 -0500

    added model

---
+++ Model.swift
@@ -0,0 +1,31 @@
+//
+//  Model.swift
+//  Demo_fetchData
+//
+//  Created by Jian Ma on 11/19/20.
+//
+
+import Foundation
+
+struct MoviesData: Decodable{
+    let movies: [Movie]
+    
+    private enum CodingKeys: String, CodingKey{
+        case movies = "reskults"
+    }
+}
+
+struct Movie: Decodable{
+    let title: String?
+    let year: String?
+    let rate: Double?
+    let posterImage: String?
+    let overview: String?
+    
+    private enum Codingkeys: String, CodingKey {
+        case title, overview
+        case year = "release_data"
+        case rate = "vote_averge"
+        case posterImage = "poster_path"
+    }
+}
