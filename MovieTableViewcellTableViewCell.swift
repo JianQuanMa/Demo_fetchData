43162c1e376dba1d935e7bbc88defd5d28194df2
Jian Ma <jianquanma@gmail.com>
2020-11-20 23:25:07 -0500

    finished the auto constraints for the tableView and tableViewCell

---
+++ MovieTableViewcellTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieTableViewcellTableViewCell.swift
+//  Demo_fetchData
+//
+//  Created by Jian Ma on 11/20/20.
+//
+
+import UIKit
+
+class MovieTableViewcellTableViewCell: UITableViewCell {
+
+  
+
+}
